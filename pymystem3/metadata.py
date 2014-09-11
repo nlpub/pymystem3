03bc27b0d6c0e1e02662f3f8f87bd53772968acf
Denis Sukhonin <d.sukhonin@gmail.com>
2014-09-11 19:47:08 +0400

    Release 0.1.1

--- pymystem3/metadata.py
+++ pymystem3/metadata.py
@@ -8,11 +8,11 @@ Information describing the project.
 package = 'pymystem3'
 project = "Python MyStem 3"
 project_no_spaces = project.replace(' ', '-')
-version = '0.1.0'
+version = '0.1.1'
 description = 'Python wrapper for the Yandex MyStem 3'
 authors = ['Denis Sukhonin']
 authors_string = ', '.join(authors)
 emails = ['d.sukhonin@gmail.com']
 license = 'MIT'
-copyright = '2013 ' + authors_string
+copyright = '2013, 2014 ' + authors_string
 url = 'https://github.com/Digsolab/pymystem3'
