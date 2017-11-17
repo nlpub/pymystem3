edda6f164e2e5ef33d1f094cb17780160d9211b0
Alexander Panchenko <panchenko.alexander@gmail.com>
2017-11-17 15:58:11 +0100

    revert the pipeline mode for windows fix b16f4d

--- pymystem3/mystem.py
+++ pymystem3/mystem.py
@@ -43,11 +43,11 @@ _TARBALL_URLS = {
 _NL = unicode('\n').encode('utf-8')
 _POSIX = os.name == 'posix'
 
-_PIPELINE_MODE = (
-    '__pypy__' not in sys.builtin_module_names
-    or sys.pypy_version_info >= (2, 5, 0)
-)
-
+_PIPELINE_MODE = False
+if _POSIX and '__pypy__' in sys.builtin_module_names:
+    _PIPELINE_MODE = sys.pypy_version_info >= (2, 5, 0)
+elif _POSIX:
+    _PIPELINE_MODE = True
 
 def autoinstall(out=sys.stderr):
     """
