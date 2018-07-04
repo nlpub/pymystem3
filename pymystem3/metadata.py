1d9e50f789bab0e9a6e3362cfd8a84dd26a3b447
Alexander Panchenko <panchenko.alexander@gmail.com>
2018-07-04 18:41:01 +0300

    bump to 0.2.0 - mystem 3.1 support

--- pymystem3/metadata.py
+++ pymystem3/metadata.py
@@ -8,7 +8,7 @@ Information describing the project.
 package = 'pymystem3'
 project = "Python MyStem 3"
 project_no_spaces = project.replace(' ', '-')
-version = '0.1.10'
+version = '0.2.0'
 description = 'Python wrapper for the Yandex MyStem 3.1 morpholocial analyzer of the Russian language.'
 authors = ['Denis Sukhonin', 'Alexander Panchenko', 'Daniel Brshatsky', 'Andy Fefelov', 'Anton Malashenkov']
 authors_string = ', '.join(authors)
